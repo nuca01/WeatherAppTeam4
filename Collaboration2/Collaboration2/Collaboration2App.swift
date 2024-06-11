541b10d910b637301a1eca1d5e5fd5ff582c4210
nuca01 <nucaberiashvili16@gmail.com>
2024-06-11 23:19:46 +0400

    create initial project

---
+++ Collaboration2/Collaboration2/Collaboration2App.swift
@@ -0,0 +1,32 @@
+//
+//  Collaboration2App.swift
+//  Collaboration2
+//
+//  Created by nuca on 11.06.24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Collaboration2App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Collaboration2/Collaboration2/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Collaboration2
+//
+//  Created by nuca on 11.06.24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}

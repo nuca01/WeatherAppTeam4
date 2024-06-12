75ab4fe9757556af949ad781a3b729b47c5048c2
vanokvakhadze <vkvakhadze35@gmail.com>
2024-06-12 16:29:31 +0400

    Rain

--- Collaboration2/Collaboration2/Scenes/Weather Page/WeatherPageView.swift
+++ Collaboration2/Collaboration2/Scenes/Weather Page/WeatherPageView.swift
@@ -6,19 +6,42 @@
 //
 
 import SwiftUI
+import SpriteKit
 
 struct WeatherPageView: View {
     @ObservedObject var viewModel: WeatherPageViewModel
     @State var city: City?
     
     var body: some View {
-        Text("Hello, World!")
+        ZStack{
+            Text("Hello, World!")
+        }
+        SpriteView(scene: RainFail(), options: [.allowsTransparency])
     }
     
     init(viewModel: WeatherPageViewModel) {
         self.viewModel = viewModel
     }
 }
+
+class RainFail: SKScene{
+    override func sceneDidLoad() {
+        
+        size = UIScreen.main.bounds.size
+        
+        scaleMode = .resizeFill
+        
+        anchorPoint = CGPoint(x: 1 , y: 1)
+        
+        backgroundColor = .clear
+        
+        let node = SKEmitterNode(fileNamed: "RainFall.sks")!
+        
+        addChild(node)
+        node.position = CGPoint(x: 1, y: 1)
+    }
+}
+
 
 #Preview {
     WeatherPageView(viewModel: WeatherPageViewModel())

9773852e92fedf8ff96c39b1f4c9bc80eb6abed5
GiorgiMM16 <michitashvili.giorgi3@gmail.com>
2024-06-12 17:22:04 +0400

    Upload Birds and Sun animation

---
+++ Collaboration2/Collaboration2/Scenes/Weather Page/Sun, Birds, Clouds/Birds.swift
@@ -0,0 +1,18 @@
+//
+//  Birds.swift
+//  Collaboration2
+//
+//  Created by Giorgi Michitashvili on 6/12/24.
+//
+
+import SwiftUI
+
+struct Birds: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Birds()
+}

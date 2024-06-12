0bc307f2715382ba6ec7affba9e40c00a04363ed
nuca01 <nucaberiashvili16@gmail.com>
2024-06-12 13:08:34 +0400

    crate CitiesList struct

--- Collaboration2/Collaboration2/Scenes/Search Page/CitiesList.swift
+++ Collaboration2/Collaboration2/Scenes/Search Page/CitiesList.swift
@@ -5,4 +5,21 @@
 //  Created by nuca on 12.06.24.
 //
 
-import Foundation
+import SwiftUI
+
+struct CitiesList: View {
+    var list: [City]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            ForEach(list) { city in
+                HStack {
+                    Text(city.name ?? "city name unavailable")
+                        .padding(EdgeInsets(top: 10, leading: 10, bottom: 0, trailing: 10))
+                    
+                    Spacer()
+                }
+            }
+        }
+    }
+}
